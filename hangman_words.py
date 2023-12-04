1b4fcc9e59fba4d261ea41de7f67d89d1620cd1d
Arkaghoshg <144145423+Arkaghoshg@users.noreply.github.com>
2023-12-04 14:37:20 +0530

    Add files via upload

---
+++ hangman_words.py
@@ -0,0 +1,7 @@
+word_list = {
+    "cat": "A furry feline",
+    "dog": "A loyal canine",
+    "bird": "An animal that can fly",
+    "fish": "An animal that lives in water",
+    "horse": "A large mammal that can be ridden"
+}
